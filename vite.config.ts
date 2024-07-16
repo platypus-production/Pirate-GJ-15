8276d3f082b9dbdb145e2f71926d3bf91016956d
Allan <allan.cerveaux@solotech.com>
2024-07-16 13:47:21 +0200

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 1337,
-    host: 'localhost'
-  },
-  preview: {
+	plugins: [viteTsConfigPaths()],
+	base: process.env.NODE_ENV === "production" ? "/Pirate-GJ-15/" : undefined,
+	server: {
+		port: 1337,
+		host: "localhost",
+	},
+	preview: {
 		port: 4400,
 		host: "localhost",
 	},
-
-  plugins: [
-    viteTsConfigPaths(),
-  ],
-
-	build: {
-		reportCompressedSize: true,
-		commonjsOptions: {
-			transformMixedEsModules: true,
-		},
-	},
-})
+});
