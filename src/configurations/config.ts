999021c87f06b3fd99cc10d60c20ed213b6a6c98
Allan <allan.cerveaux@solotech.com>
2024-07-20 20:23:16 +0200

    the first step of adding i18n

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -1,17 +1,18 @@
 import { DEFAULT_HEIGHT, DEFAULT_PHYSICS, DEFAULT_WIDTH } from "@constants";
+import { phaserI18n } from "@plugins";
 import scenes from "@scenes";
+import resources from "../assets/i18n";
 
 export const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	backgroundColor: "#1E1E1E",
-	fps: {
-		smoothStep: false,
-	},
 	render: {
 		pixelArt: true,
 	},
 	scene: scenes,
-	plugins: {},
+	plugins: {
+		global: [phaserI18n({ resources })],
+	},
 	scale: {
 		parent: "phaser-game",
 		mode: Phaser.Scale.FIT,
@@ -23,7 +24,6 @@ export const config: Phaser.Types.Core.GameConfig = {
 		default: DEFAULT_PHYSICS,
 		[DEFAULT_PHYSICS]: {
 			debug: process.env.NODE_ENV === "development",
-			overlapBias: 8,
 		},
 	},
 };
