b0ac8122e4fef04b37ffa22913ecde661b27be21
Allan <allan.cerveaux@solotech.com>
2024-07-21 16:17:58 +0200

    REMOVE: Default keys mapping into InputManager

--- src/configurations/constants.ts
+++ src/configurations/constants.ts
@@ -2,14 +2,6 @@ export const DEFAULT_PHYSICS: "matter" | "arcade" = "arcade";
 export const DEFAULT_WIDTH = 1280;
 export const DEFAULT_HEIGHT = 720;
 
-export const DEFAULT_INPUT_KEYS = {
-	left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-	right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-	up: Phaser.Input.Keyboard.KeyCodes.UP,
-	down: Phaser.Input.Keyboard.KeyCodes.DOWN,
-	pause: Phaser.Input.Keyboard.KeyCodes.P,
-};
-
 export enum SCENES {
 	PRELOAD = "preload-scene",
 	MENU = "menu-scene",
